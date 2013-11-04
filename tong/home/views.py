1c37e7f32a8f6dc192429eac0297126f2b0f78e2
Tong Da <tongda@outlook.com>
2013-11-05 00:10:07 +0800

    Add earth library.

--- tong/home/views.py
+++ tong/home/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-	return HttpResponse('Hello, I\'m Tong Da, and I like Pei Pei.')
+	return render(request, 'home/index.html')

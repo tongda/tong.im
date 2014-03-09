1bc1dd1660ce7960a8052cd7bfe305d0ad2e3407
Tong Da <tongda@outlook.com>
2014-03-09 23:33:19 +0800

    to fit pep8

--- tong/tong/urls.py
+++ tong/tong/urls.py
@@ -4,17 +4,10 @@ from django.conf.urls import patterns, include, url
 # from django.contrib import admin
 # admin.autodiscover()
 
-urlpatterns = patterns('',
-                       url(r'^$', 'tong.views.get_home', name='home'),
-                       url(r'^home/', include('tong.home.urls', namespace='home')),
-                       url(r'^blog/', include('tong.blog.urls', namespace='home')),
-                       url(r'^about/', include('tong.about.urls', namespace='home')),
-                       # Examples:
-                       # url(r'^$', 'tong.views.home', name='home'),
-                       # url(r'^tong/', include('tong.foo.urls')),
-                       # Uncomment the admin/doc line below to enable admin documentation:
-                       # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-                       # Uncomment the next line to enable the admin:
-                       # url(r'^admin/', include(admin.site.urls)),
-                       )
+urlpatterns = patterns(
+    '',
+    url(r'^$', 'tong.views.get_home', name='home'),
+    url(r'^home/', include('tong.home.urls', namespace='home')),
+    url(r'^blog/', include('tong.blog.urls', namespace='home')),
+    url(r'^about/', include('tong.about.urls', namespace='home')),
+    )
